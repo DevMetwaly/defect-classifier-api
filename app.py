df05c6679816587e9508aaf9814950278c0a50ae
spearkiller0 <abdohamdymetwaly@gmail.com>
2021-01-11 20:17:48 +0200

    working on colored images

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ def detectEndPoint():
     return defects, 200
 
   except Exception as e:
+    print(e)
     res = {
       "status":"faild",
       "message":"Some thing wrong!, try again later"
@@ -136,6 +137,61 @@ def getImage():
     "status":"faild",
     "message":"Some thing wrong!, try again later and make sure ImgaeId is correct"
     #"exception":str(e)
+    }
+    return res, 500
+
+def convertToBw(path):
+  im_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+  thresh = 127
+  im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+  im_bw = cv2.bitwise_not(im_bw)
+  cv2.imwrite(path, im_bw)
+  
+@app.route("/Detect/colored", methods=["GET"])
+def detectColoredEndPoint():
+  try:
+    temp_img= request.files["temp_image"]
+    test_img= request.files["test_image"]
+    temp_img.save(os.path.join("test_temps", "temp_img.png"))
+    test_img.save(os.path.join("test_temps", "test_img.png"))
+    print(os.path.join("test_temps", "test_img.png"))
+    convertToBw(os.path.join("test_temps", "temp_img.png"))
+    convertToBw(os.path.join("test_temps", "test_img.png"))
+    
+
+    id = "ID"
+
+    image_differencing("test_temps/temp_img.png","test_temps/test_img.png",id)
+    extract_defects_using_contours("diff_img/diff_" + id + ".png",id)
+
+    cnt = 0 # number of defects
+    defects = {
+      "status": "success",
+      "list":[]
+    }
+    images = os.listdir("contours/"+id)
+    for imageName in images:
+      if("big" in imageName):
+        continue
+      image_defect_type = predict("contours/"+id+"/"+imageName)
+      type = image_defect_type["Category"]
+      if type != "not_defect":
+        img_def = {
+          "ImageId": imageName[:-4],
+          "type": type,
+          "solutionUrl":sol_urls[type]
+        }
+        defects["list"].append(img_def)
+        cnt+=1
+    defects["numberOfDefects"] = cnt
+    return defects, 200
+
+  except Exception as e:
+    print(e)
+    res = {
+      "status":"faild",
+      "message":"Some thing wrong!, try again later"
+      #"exception":str(e)
     }
     return res, 500
 if __name__ == "__main__":
